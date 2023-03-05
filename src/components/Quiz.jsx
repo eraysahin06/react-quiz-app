063620258e29f215639435d4f356e34e294667ae
Eray Sahin <eraysahinmoon@gmail.com>
2023-03-04 19:00:13 -0500

    state hook added (money)

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,11 @@
+export default function Quiz() {
+  return (
+    <div className="quiz">
+        <div className="question">What's the best YouTube channel?</div>
+        <div className="answer">LamaDev</div>
+        <div className="answer">LamaDev</div>
+        <div className="answer">LamaDev</div>
+        <div className="answer">LamaDev</div>
+    </div>
+  )
+}
